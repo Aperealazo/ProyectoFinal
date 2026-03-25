7ce5f5f9a6582f6bc828f4a6daf8aa635c70e493
Alex P.L <111670520+Aperealazo@users.noreply.github.com>
2026-03-25 11:17:19 -0300

    Hola Github soy Yo

---
+++ comandosFlujos.js
@@ -0,0 +1,22 @@
+// Comandos de flujo basico[Subir al repo]
+/** Pasos
+ * 1-  git add .
+ * 2-  git commit -m "Comentan el cambio que hicieron"
+ * 3-  git push  
+ **/
+
+//=======================================================
+// SIGNIFICADO DE LOS COMANDOS.
+//=======================================================
+/**
+ * add -> Prepara los cambios
+ * commit -m -> Guarda una version
+ * push -> Sube el cambio a gitHub
+ */
+//=======================================================
+// Atajos...
+//=======================================================
+/**
+ * 1- Ctrl + Shift + P -> Para Clonar el repo. Aqui se coloca la url del repo
+ * 2- Ctrl + ñ -> Abre la terminal del vs para poner los comandos.
+ */
